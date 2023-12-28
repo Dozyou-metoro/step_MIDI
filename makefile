92f706e0be4fbeda29f7ad9a4900706c778ea2a0
Kouno-metoro <KounoMetoro_0000@outlook.jp>
2023-12-28 12:20:48 +0900

    安定した

--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 step_MIDI:step_MIDI.cpp
-	g++ step_MIDI.cpp -o step_MIDI -lpigpio --std=c++17 -g -Wall
+	g++ step_MIDI.cpp -o step_MIDI -lpigpio --std=c++17 -g -Wall 
--- step_MIDI.cpp
+++ step_MIDI.cpp
@@ -13,11 +13,12 @@
 #include "../MIDIfile_io/tone_name.hpp"
 #include "../MIDIfile_io/MIDI_input.hpp"
 
-
 const uint32_t spi_speed = 1000000;
 const uint8_t spi_ch = 0;
 const uint8_t spi_ss1_pin = 25;
 const uint8_t spi_ss2_pin = 8;
+const uint8_t spi_busy1_pin=2;
+const uint8_t spi_busy2_pin=3;
 
 class step_MIDI_move
 {
@@ -27,27 +28,30 @@ private:
     uint32_t tempo_us = 0;                  // テンポ(四分音符あたりus)
     MIDI_input *midi;                       // MIDIクラスを入れておく
 
+
+
 public:
     template <typename... TYPE>
-    step_MIDI_move(MIDI_input *midi_class, TYPE... ss_pin);
+    step_MIDI_move(MIDI_input *midi_class, TYPE... pins);
 
     ~step_MIDI_move();
 
     void step_play(MIDI_track_data::track_data_format MIDI_data);
 
     stepper_motor *motor;
+
 };
 
 template <typename... TYPE>
-step_MIDI_move::step_MIDI_move(MIDI_input *midi_class, TYPE... ss_pin)
+step_MIDI_move::step_MIDI_move(MIDI_input *midi_class, TYPE... pins)
 {
-    motor = new stepper_motor(spi_speed, spi_ch, ss_pin...); // ステッピングモーター駆動クラスを定義
+    motor = new stepper_motor(spi_speed, spi_ch, pins...); // ステッピングモーター駆動クラスを定義
 
     for (int i = 0; i < motor->get_motor_num(); i++)
     {
         motor->send_data(i, 0x00, 0x00, 0x00, 0x00);
 
-        motor->send_data(i, 0x05, 0x02, 0x20);
+        motor->send_data(i, 0x05, 0x01, 0x00);
         motor->send_data(i, 0x06, 0x02, 0x20);
         motor->send_data(i, 0x07, 0x01, 0xC4);
         motor->send_data(i, 0x08, 0x00, 0x00);
@@ -88,11 +92,16 @@ step_MIDI_move::step_MIDI_move(MIDI_input *midi_class, TYPE... ss_pin)
                 printf("TEMPO:%" PRIx32 "\n", tempo_us);
             }
         }
+
+       
     }
+
+    
 }
 
 step_MIDI_move::~step_MIDI_move()
 {
+
     delete motor;
 }
 
@@ -107,7 +116,7 @@ void step_MIDI_move::step_play(MIDI_track_data::track_data_format MIDI_data)
             {
                 motor_busy_status[i] = 0;
                 std::this_thread::sleep_for(std::chrono::microseconds((int)(tempo_us * ((double)MIDI_data.time / midi->heard_data->resolution))));
-                motor->send_data(i, 0x51, (uint8_t)0x00, (uint8_t)0x00, (uint8_t)0x00);
+                motor->send_data(i, 0xB0);
 
                 printf("MOTOR:%d ", i);
                 for (int j = 0; j < MIDI_data.data_size; j++)
@@ -129,7 +138,7 @@ void step_MIDI_move::step_play(MIDI_track_data::track_data_format MIDI_data)
             {
                 motor_busy_status[i] = MIDI_data.event_data[1];
                 std::this_thread::sleep_for(std::chrono::microseconds((int)(tempo_us * ((double)MIDI_data.time / midi->heard_data->resolution))));
-
+                
                 motor->send_data(i, 0x51, ((int)(66 * tone.tone_frequency[MIDI_data.event_data[1]]) >> 16) & 0xff, ((int)(66 * tone.tone_frequency[MIDI_data.event_data[1]]) >> 8) & 0xff, (int)(66 * tone.tone_frequency[MIDI_data.event_data[1]]) & 0xff);
 
                 printf("MOTOR:%d ", i);
@@ -155,6 +164,7 @@ void step_MIDI_move::step_play(MIDI_track_data::track_data_format MIDI_data)
     }
 }
 
+
 int main(int argc, char **argv)
 {
     if (argc < 2)
@@ -164,7 +174,9 @@ int main(int argc, char **argv)
 
     MIDI_input midi(argv[1]);
 
-    step_MIDI_move step(&midi, spi_ss1_pin, spi_ss2_pin);
+    step_MIDI_move step(&midi, spi_ss1_pin, spi_busy1_pin,spi_ss2_pin,spi_busy2_pin);
+
+
 
     for (int i = 0;; i++)
     {
